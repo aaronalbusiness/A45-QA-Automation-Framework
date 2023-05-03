a64ac33328ec4b837358bfa583ed4392f3a58efa
aaronalbusiness <aaronalbusiness@gmail.com>
2023-05-03 12:18:12 -0400

    Update

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 public class BaseTest {
 
     public static WebDriverWait wait;
-    //public static WebDriver driver= null;
+    public static WebDriver driver= null;
 
    public static String url = "";
 
