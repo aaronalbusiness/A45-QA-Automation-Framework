18ffe2f3e03bff17cb72158a539e5e1300931f2f
aaronalbusiness <aaronalbusiness@gmail.com>
2023-05-11 10:12:06 -0400

    clean up thread sleep

--- src/test/java/AddSongToPlaylistTest.java
+++ src/test/java/AddSongToPlaylistTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 public class AddSongToPlaylistTest extends BaseTest {
 
     @Test
-    public void addSongToPlaylist() throws InterruptedException {
+    public void addSongToPlaylist() {
         String newSongAddedNotificationText = "Added 1 song into";
 
         login("demo@class.com", "te$t$tudent");
