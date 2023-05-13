25d61f65db7e547d841092ceba88fc732849a188
aaronalbusiness <aaronalbusiness@gmail.com>
2023-05-13 13:40:47 -0400

    clean up

---
+++ src/test/java/PlaySongTest.java
@@ -0,0 +1,20 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+public class PlaySongTest extends BaseTest{
+
+    //private WebDriver driver;
+
+    @Test
+    public void playSongTest()  {
+        login("demo@class.com", "te$t$tudent");
+        chooseAllSongList();
+        contextClickFirstSong();
+        playSong();
+        Assert.assertTrue(isSongPlaying());
+    }
+
+}
+
